f1916e24f97521248be359d05531ee3348bd168e
miftahaziz <al_aziz@node8.net>
2017-11-15 17:07:36 +0700

    first stuff

--- simple_crud_api/settings.py
+++ simple_crud_api/settings.py
@@ -83,13 +83,13 @@ WSGI_APPLICATION = 'simple_crud_api.wsgi.application'
 # #This did the trick
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_USE_TLS = False
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'miftahulaziz92@gmail.com'
 EMAIL_HOST_PASSWORD = 'sangkubutut'
-EMAIL_PORT = 25
-# EMAIL_PORT = 587
+# EMAIL_PORT = 25
+EMAIL_PORT = 587
 # EMAIL_PORT = 465
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
