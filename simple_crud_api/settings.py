ac9c6fcbd0b1a635ab6783bc313e7b9de2975624
miftahaziz <al_aziz@node8.net>
2017-11-15 17:30:53 +0700

    first stuff

--- simple_crud_api/settings.py
+++ simple_crud_api/settings.py
@@ -95,7 +95,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'miftahulaziz92@gmail.com'
 EMAIL_HOST_PASSWORD = 'sangkubutut'
 EMAIL_USE_TLS = True
