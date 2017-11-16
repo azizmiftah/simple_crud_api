49512a2d43135e028718fffbd9603a513ed5eba2
miftahaziz <al_aziz@node8.net>
2017-11-16 15:06:06 +0700

    add article model

--- app/serializers.py
+++ app/serializers.py
@@ -8,6 +8,12 @@ class UserSerial(serializers.ModelSerializer):
 		depth = 1
 		fields = '__all__'
 
+class ArticleSerial(serializers.ModelSerializer):
+	class Meta:
+		model = Article
+		depth = 1
+		fields = '__all__'
+
 class ParamObtainToken(serializers.ModelSerializer):
 	class Meta:
 		model = User
