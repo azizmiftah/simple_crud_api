4a587dc7495b5eb5f9548e533d694724bfd0838e
miftahaziz <al_aziz@node8.net>
2017-11-17 15:08:48 +0700

    add article model

--- app/views.py
+++ app/views.py
@@ -96,7 +96,7 @@ class RegisterView(APIView):
 	def post(self, request):
 		serial = UserSerial(data=request.data)
 		if serial.is_valid():
-			code = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits + '!@#$()&?><{}[]') for _ in range(30))
+			code = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(30))
 			send_mail(to=[serial.validated_data['email']], title='Simple CRUD Account Info', body='click this activation link: ' + "http://localhost/tugas_ptbig/activation.html?activation_code=" + code)
 			serial.save(activate_code=code, password=make_password(password=request.data['password'], salt=None))
 			return Response({"result":serial.data,"message":"OK"})
